b40e8fce9285f0643a4e0e317d60aeb1bd3ba69f
afernandez0196 <afernandez0196@hotmail.com>
2017-12-03 12:46:47 -0600

    Añadiendo Método Agregar
    Se añade el método Agregar.
    Añade por parámetro: UserID, Email, Name y SurName a la base de datos.

--- NutriApp/NutriApp/Controllers/UserAdminController.cs
+++ NutriApp/NutriApp/Controllers/UserAdminController.cs
@@ -17,6 +17,26 @@ namespace NutriApp.Controllers
             ViewBag.Measures = db.Measures.ToList();
             return View(lstUsers);
         }
+        
+        public ActionResult Agregar(string UserId, string Email, string Name, string SurName)
+        {
+            NutriAppsEntities db = new NutriAppsEntities();
+
+            string sql = "INSERT INTO dbo.Users(UserID,UserRole,UserType,Password,Email,Name,Surname) VALUES(@UserId,'Client','2','Temp!01',@Email,@Name,@SurName)";
+            List<SqlParameter> parameterList = new List<SqlParameter>();
+            parameterList.Add(new SqlParameter("@UserId", UserId));
+            parameterList.Add(new SqlParameter("@Email", Email));
+            parameterList.Add(new SqlParameter("@Name", Name));
+            parameterList.Add(new SqlParameter("@SurName", SurName));
+            SqlParameter[] parameters = parameterList.ToArray();
+            int result = db.Database.ExecuteSqlCommand(sql, parameters);
+
+            //db.Groups.SqlQuery("INSERT INTO dbo.Groups(Topic,UserID,GroupID,Description,Group_Type) VALUES('" + Topic + "','" + UserId + "','" + GroupId + "','" + Description + "','" + GroupType + "')");
+            List<User> lstUsers = db.Users.ToList();
+
+            ViewBag.Measures = db.Measures.ToList();
+            return View("Index", lstUsers);
+        }
 
         public ActionResult About()
         {
