48bb328055a2318f26df4d091c3fa3f22e10a380
afernandez0196 <afernandez0196@hotmail.com>
2017-12-03 13:06:11 -0600

    Validaciones Metodo Agregar
    Verifica en la base de datos si exite; sino procede a añadirlo a la misma

--- NutriApp/NutriApp/Controllers/UserAdminController.cs
+++ NutriApp/NutriApp/Controllers/UserAdminController.cs
@@ -21,21 +21,31 @@ namespace NutriApp.Controllers
         public ActionResult Agregar(string UserId, string Email, string Name, string SurName)
         {
             NutriAppsEntities db = new NutriAppsEntities();
+            User user = null;
+            user = db.Users.Single(x => x.UserID == Username);
 
-            string sql = "INSERT INTO dbo.Users(UserID,UserRole,UserType,Password,Email,Name,Surname) VALUES(@UserId,'Client','2','Temp!01',@Email,@Name,@SurName)";
-            List<SqlParameter> parameterList = new List<SqlParameter>();
-            parameterList.Add(new SqlParameter("@UserId", UserId));
-            parameterList.Add(new SqlParameter("@Email", Email));
-            parameterList.Add(new SqlParameter("@Name", Name));
-            parameterList.Add(new SqlParameter("@SurName", SurName));
-            SqlParameter[] parameters = parameterList.ToArray();
-            int result = db.Database.ExecuteSqlCommand(sql, parameters);
+            if (user != null)
+            {
+                return View("<script language='javascript' type='text/javascript'>alert('Username already exist. Please select another username');</script>", "Register");
+            }
+            else
+            {
+                string sql = "INSERT INTO dbo.Users(UserID,UserRole,UserType,Password,Email,Name,Surname) VALUES(@UserId,'Client','2','Temp!01',@Email,@Name,@SurName)";
+                List<SqlParameter> parameterList = new List<SqlParameter>();
+                parameterList.Add(new SqlParameter("@UserId", UserId));
+                parameterList.Add(new SqlParameter("@Email", Email));
+                parameterList.Add(new SqlParameter("@Name", Name));
+                parameterList.Add(new SqlParameter("@SurName", SurName));
+                SqlParameter[] parameters = parameterList.ToArray();
+                int result = db.Database.ExecuteSqlCommand(sql, parameters);
 
-            //db.Groups.SqlQuery("INSERT INTO dbo.Groups(Topic,UserID,GroupID,Description,Group_Type) VALUES('" + Topic + "','" + UserId + "','" + GroupId + "','" + Description + "','" + GroupType + "')");
-            List<User> lstUsers = db.Users.ToList();
+                //db.Groups.SqlQuery("INSERT INTO dbo.Groups(Topic,UserID,GroupID,Description,Group_Type) VALUES('" + Topic + "','" + UserId + "','" + GroupId + "','" + Description + "','" + GroupType + "')");
+                List<User> lstUsers = db.Users.ToList();
 
-            ViewBag.Measures = db.Measures.ToList();
-            return View("Index", lstUsers);
+                ViewBag.Measures = db.Measures.ToList();
+                return View("Index", lstUsers);
+            }
+
         }
 
         public ActionResult About()
